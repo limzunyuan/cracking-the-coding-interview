62e29e8872a6b8a6c66a7bdf0db49a55dbb8612d
Zun yuan Lim <limzunyuan@hotmail.com>
2018-11-17 21:30:45 +0000

    fix in place rotate

--- 1.7_6thedit.cpp
+++ 1.7_6thedit.cpp
@@ -1,12 +1,13 @@
 void rotateMatric(std::vector<std::vector<int>>& mat) {
-	int m = mat.size();
-	for(int i = 0; i < (m+1)/2; ++i) {
-		for(int j = 0; j < (m+1)/2; ++j) {
-			int tmp = mat[i][j];
-			mat[i][j] = mat[i][m-1-j];
-			mat[i][m-1-j] = mat[m-1-i][m-1-j];
-			mat[m-i-1][m-1-j] = mat[m-i-1][j];
-			mat[m-i-1][j] = tmp;
-		}
-	}
+        int m = mat.size();
+        int tmp;
+        for(int i = 0; i < m/2; ++i) {
+            for(int j = 0; j < (m+1)/2; ++j) {
+                tmp = mat[i][j];
+                mat[i][j] = mat[m-1-j][i];
+                mat[m-j-1][i] = mat[m-1-i][m-1-j];
+                mat[m-i-1][m-1-j] = mat[j][m-1-i];
+                mat[j][m-1-i] = tmp;
+            }
+        }
 }
